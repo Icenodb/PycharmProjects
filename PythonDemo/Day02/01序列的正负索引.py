236a6523be585b2814df80db2fb72ff7bddece91
Icenodb <Icenodb@gmail.com>
2021-08-10 16:35:10 +0800

    06序列的切片

--- PythonDemo/Day02/01序列的正负索引.py
+++ PythonDemo/Day02/01序列的正负索引.py
@@ -1,11 +1,13 @@
 #[]
 seq1=[1,2,3,4,5,6,7,8,9]
 #基于正数索引读取元素
-val1=seq1[0]
-print(val1)
-print(seq1[2])
-
-#负索引
-print(seq1[-0])
-print(seq1[-1])
+# val1=seq1[0]
+# print(val1)
+# print(seq1[2])
+print(seq1[0:4])#截取操作
+print(seq1[0:-1])#截取到最后一位(不含)
+print(seq1[2:])#从第三个元素开始到最后的所有元素
+# #负索引
+# print(seq1[-0])
+# print(seq1[-1])
 
