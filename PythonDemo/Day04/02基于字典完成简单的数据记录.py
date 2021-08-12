4552d46e9106d3827b59dbb7474fcdc9c5734472
Icenodb <Icenodb@gmail.com>
2021-08-12 14:34:01 +0800

    Update 02基于字典完成简单的数据记录.py

--- PythonDemo/Day04/02基于字典完成简单的数据记录.py
+++ PythonDemo/Day04/02基于字典完成简单的数据记录.py
@@ -13,7 +13,7 @@
 # print(people['王兴刚']['phone'])
 
 
-#录入数据
+# 录入数据
 # people={}
 # while True:
 #     info=input("请输入用户信息:uname,phone,address?")
@@ -22,6 +22,30 @@
 #     people[list_info[0]]={"phone":list_info[1],"address":list_info[2]}
 # print(people)
 
-db={
-    
+db = {
+    '王兴刚':
+        {'phone': '110', 'address': '中国.青岛'},
+    '岳云鹏':
+        {'phone': '50', 'address': '活着我心中'},
+    '毛惊奇':
+        {'phone': '119', 'address': '中国.四川.巴中'}
 }
+
+lable = {"a": "address", "p": "phone"}
+while True:
+    uname=input("E请输入姓名:")
+    if uname.lower()=="q":break
+
+    if not uname in db:
+        print(f"找不到名为'{uname}'的用户")
+        continue
+
+    while True:
+        key=input(f"请输入'{uname}'的phone(p) 或者address(a):")
+        if not key in lable:
+            print(f"公民 {uname},没有登记 与{key}相关的信息")
+            continue
+        else:
+            key=lable[key]
+            print(f"公民{uname}的{key}信息如下:{db[uname][key]}")
+            break
