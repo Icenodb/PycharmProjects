05d2d8a7f1e8a5e729ad8a12ae7fb4ace371da32
Icenodb <Icenodb@gmail.com>
2021-08-12 14:21:58 +0800

    02基于字典的数据记录

---
+++ PythonDemo/Day04/02基于字典完成简单的数据记录.py
@@ -0,0 +1,27 @@
+# people={}
+# # people['ename']='王兴刚'  #添加键值对
+# # people['ename']='郭德纲'  #更新键值对
+# # print(people['ename'])   #基于键读取值
+# people['王兴刚']={'phone':'110','address':'中国.青岛'}
+# people['岳云鹏']={'phone':'50','address':'活着我心中'}
+# print(people)  #显示全部数据
+# print('-----------------------------------------')
+# #显示王兴刚的信息
+# print(people['王兴刚'])
+# print('-----------------------------------------')
+# #获取王兴刚的手机号码
+# print(people['王兴刚']['phone'])
+
+
+#录入数据
+# people={}
+# while True:
+#     info=input("请输入用户信息:uname,phone,address?")
+#     if info.lower()=="q":break
+#     list_info=info.split(",")
+#     people[list_info[0]]={"phone":list_info[1],"address":list_info[2]}
+# print(people)
+
+db={
+    
+}
