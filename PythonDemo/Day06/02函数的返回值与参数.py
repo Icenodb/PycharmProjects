adc1fb1c0f51c0ffde90be983665ecff540d3154
Icenodb <Icenodb@gmail.com>
2021-08-14 15:58:09 +0800

    Update 02函数的返回值与参数.py

--- PythonDemo/Day06/02函数的返回值与参数.py
+++ PythonDemo/Day06/02函数的返回值与参数.py
@@ -1,15 +1,15 @@
-
-def fun_fibs(n:int):
+def fun_fibs(n: int):
     """
     :param n:返回斐波那契数列前n个数
     :return:
     """
-    fibs=[1,1]
-    for m in range(3,n+1):
-        fibs.append(fibs[-1]+fibs[-2])
+    fibs = [1, 1]
+    for m in range(3, n + 1):
+        fibs.append(fibs[-1] + fibs[-2])
     return fibs
 
-def formatFibs(data:list,rowCount:int):
+
+def formatFibs(data: list, rowCount: int):
     """
     :param data:传入列表
     :param rowCount:几个数换行
@@ -19,10 +19,10 @@ def formatFibs(data:list,rowCount:int):
     for p in data:
         print(f"{p:10}", end=",")
         index += 1
-        if (index % rowCount == 0): print()
+        if index % rowCount == 0: print()
     print("--------------------------------------")
 
 
 if __name__ == '__main__':
-    fibsList=fun_fibs(36)
-    formatFibs(fibsList,6)
+    fibsList = fun_fibs(36)
+    formatFibs(fibsList, 6)
