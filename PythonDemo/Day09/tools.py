650b8eb6971a7ec10bfde10facc9dbb414342d47
Icenodb <Icenodb@gmail.com>
2021-08-19 15:43:51 +0800

    Create tools.py

---
+++ PythonDemo/Day09/tools.py
@@ -0,0 +1,10 @@
+def strToNumber(value: str):
+    return float(f"{float(value):.2f}")
+
+
+def numberToNumber(value: float):
+    return float(f"{value:.2f}")
+
+
+if __name__ == '__main__':
+    print("hello this tools model.....")
