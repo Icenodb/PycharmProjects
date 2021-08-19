ccd538c09b0c157fcaad881048b486b9558ad152
Icenodb <Icenodb@gmail.com>
2021-08-19 17:26:50 +0800

    Update 01连接MySQL.py

--- PythonDemo/Day09/01连接MySQL.py
+++ PythonDemo/Day09/01连接MySQL.py
@@ -134,6 +134,112 @@ def batchDeleteEmp(eidlist: list):
     return tag
 
 
+def batchDeleteEmpAndLog(eidlist: list):
+    """多表批量删除"""
+    sql1 = "delete from emplog where eid=%s"
+    sql2 = "delete from emp where eid=%s"
+    args = [[eid] for eid in eidlist]
+    tag = False
+    with getConnection() as conn:
+        try:
+            cursor = conn.cursor()
+            cursor.executemany(sql1, args)
+            cursor.executemany(sql2, args)
+            conn.commit()
+            tag = True
+        except Exception as ex:
+            print(ex)
+            conn.rollback()
+    return tag
+
+
+def addMoney(eid: int, money: float):
+    """员工加薪，只修改员工表"""
+    sql = "update emp set sal=sal+%s where eid=%s"
+    args = [tools.strToNumber(str(money)), eid]
+    tag = False
+    with getConnection() as conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(sql, args)
+            conn.commit()
+            tag = True
+        except Exception as ex:
+            print(ex)
+            conn.rollback()
+    return tag
+
+
+def findById(eid: int):
+    """单一实例查询"""
+    sql = """
+    select eid,ename,sex,job,cast(sal as char)sal,
+    date_format(birthday,'%%Y-%%m-%%d') birthday
+    from emp where eid=%s
+    """
+    with getConnection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, [eid])
+        row = cursor.fetchone()
+    return row
+
+
+def findByIdForTuple(eid: int):
+    """单一实例查询(基于元组封装结果)"""
+    sql = """
+    select eid,ename,sex,job,cast(sal as char)sal,
+    date_format(birthday,'%%Y-%%m-%%d') birthday
+    from emp where eid=%s
+    """
+    with pymysql.Connection(host='localhost', user='root',
+                            password='lwh123', database='pydb') as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, [eid])
+        return cursor.fetchone()
+
+
+def queryAll():
+    """数据批量查询(查询全部)"""
+    sql = """
+    select eid,ename,sex,job,cast(sal as char)sal,
+    date_format(birthday,'%%Y-%%m-%%d') birthday
+    from emp limit 5
+    """
+    with getConnection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        return cursor.fetchall()
+
+
+def AddMoneyAndLog(eid: int, money: float):
+    """员工加薪，级联修改"""
+    rows=findByIdForTuple(eid)
+    args2=[rows[3],rows[4],eid]
+    print(rows)
+    print(rows[3])
+    print(rows[4])
+
+    sql1 = "update emplog set edate=current_date where eid=%s"
+    sql2 = "update emp set sal=sal+%s where eid=%s"
+    sql3 = """
+    insert into emplog(bdate,edate,job,sal,eid)
+    values(current_time,null,%s,%s,%s)
+    """
+
+    args = [tools.strToNumber(str(money)), eid]
+    tag = False
+    with getConnection() as conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(sql1, eid)
+            cursor.execute(sql2,args)
+            cursor.execute(sql3,args2)
+            conn.commit()
+            tag = True
+        except Exception as ex:
+            print(ex)
+            conn.rollback()
+    return tag
 
 # ===================MySQL连接===================
 # conn = getConnection()
@@ -154,3 +260,13 @@ def batchDeleteEmp(eidlist: list):
 
 # ===================单一实例删除==================
 # print(deleteById(1))
+# ===================单一实例查询==================
+# print(findById(2))
+# ===================单一实例查询(基于元组封装结果)==================
+# ins=findByIdForTuple(2)
+# print(ins)
+# ===================数据批量查询==================
+# print(queryAll())
+
+# ===================关联方数据修改==================
+print(AddMoneyAndLog(4,3000))
