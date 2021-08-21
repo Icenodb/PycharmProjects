a378f43ecf6c986ed7e6b8bfce3632ae25551c5c
Icenodb <Icenodb@gmail.com>
2021-08-21 14:48:59 +0800

    ++++
    +创建静态下拉列表
    +从文件读取生成下拉列表
    +读取数据库,生成下拉列表
    +创建带有滚动条的大文本域

--- PythonDemo/Day10/baseContr.py
+++ PythonDemo/Day10/baseContr.py
@@ -1,4 +1,8 @@
+import sqlite3
 import tkinter as tk
+from tkinter import ttk
+
+dbname = "../Day08/az.db"
 
 
 def newWindow(title: str = '新窗口', up: int = 5, width: int = 160, height: int = 90,
@@ -82,6 +86,112 @@ def newRadio(root, data, title='', defVal='1'):
     for tup in data:
         tk.Radiobutton(root, value=tup[1], text=tup[0], variable=value).pack(side='left')
     return value
+
+
+def newSelect(root, labels: tuple, title: str = '', defVal: int = 0):
+    """
+    创建静态下拉列表
+    :param root: 依附的容器
+    :param labels: 下拉列表的元素
+    :param title: 下拉列表的名字
+    :param defVal: 默认值
+    :return: 下拉列表对象
+    """
+    tk.Label(root, text=title).pack(side='left')
+    com = ttk.Combobox(root, state='readonly')
+    com.pack(side='left')
+    com['values'] = labels
+    com.current(defVal)
+    return com
+
+
+def newSelectReadFile(root, fileName: str, title: str = '', defVal: int = 0):
+    """
+    读取文件生成下拉列表
+    :param root: 依附的容器
+    :param fileName: 文件的名称
+    :param title: 下拉列表的标题
+    :param defVal: 默认被选中的项目的索引
+    :return: 下拉列表对象，下拉列表对象的编码元组
+    """
+    tk.Label(root, text=title).pack(side='left')
+    labels = []
+    codes = []
+    with open(fileName, 'r', encoding='utf-8') as fr:
+        for row in fr:
+            option = row.split(':')
+            codes.append(option[0])
+            labels.append(option[1])
+    com = ttk.Combobox(root, state='readonly')
+    com.pack(side=tk.LEFT)
+    com['values'] = labels
+    com.current(defVal)
+    return com, codes
+
+
+def getSelect(root, fname: str, title: str = '', defVal: int = 0):
+    """
+    读取数据库 生成下拉列表和下拉列表对象编码的元组
+    :param root:依附的容器
+    :param fname:要查询的列表名
+    :param title:标题
+    :param defVal:默认选中的值
+    :return:下拉列表对象,下拉列表对象编码的元组
+    """
+    tk.Label(root, text=title).pack(side='left')
+    # 为select控件定义code和label列表
+    labels = []
+    codes = []
+    # 定义SQL语句
+    sql = "select fcode,fvalue from syscode where isv=? and fname=?"
+    # 连接数据库
+    with sqlite3.connect(dbname) as conn:
+        # 获取游标
+        cursor = conn.cursor()
+        # 基于游标执行SQL语句
+        cursor.execute(sql, ['1', fname])
+        # 读取查询结果
+        rows = cursor.fetchall()
+    # 解析查询结果,填充select的code和label列表
+    for ins in rows:
+        codes.append(ins[0])
+        labels.append(ins[1])
+    # 创建下拉列表控件
+    com = ttk.Combobox(root, state='readonly')
+    com.pack(side=tk.LEFT)
+    # 挂接label
+    com['values'] = labels
+    # 指定默认值
+    com.current(defVal)
+    # 处理返回值
+    return com, codes
+
+
+def newText(root: tk.Frame, title: str, w: int = 45, h: int = 7):
+    """
+    创建带有滚动条的大文本域
+    :param root:依附的容器
+    :param title:标题
+    :param w:宽
+    :param h:高
+    :return:文本域对象
+    """
+    tk.Label(root, text=title).pack(side='left')
+    """大文本域"""
+    # 创建文本框容器对象
+    rowtext = tk.Frame(root)
+    # 创建文本框text，设置宽度w，文本显示的行数设置为h行
+    text = tk.Text(rowtext, width=w, font=('微软雅黑', 11), height=h)
+    text.grid(row=4, column=1, sticky=tk.S + tk.W + tk.E + tk.N)
+    # 创建滚动条
+    scroll = tk.Scrollbar(rowtext, orient="vertical", command=text.yview)
+    # 将滚动条填充
+    text.config(yscrollcommand=scroll.set)
+    # 将滚动条与文本框关联
+    scroll['command'] = text.yview
+    scroll.grid(row=4, column=2, sticky=tk.S + tk.W + tk.E + tk.N)
+    rowtext.pack(side='left')
+    return text
 
 
 if __name__ == "__main__":
