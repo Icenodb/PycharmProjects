9c053f471fc95d6eafd9065e0f82aea30bb187eb
Icenodb <Icenodb@gmail.com>
2021-08-21 14:04:28 +0800

    使用索引列表

--- PythonDemo/Day10/04下拉列表的测试.py
+++ PythonDemo/Day10/04下拉列表的测试.py
@@ -5,7 +5,7 @@ import baseContr as bc
 win = bc.newWindow()
 
 comValue = ('java', 'C', 'C++', 'Python')
-
+comCode=('01','02','03','04')        #读取文本对应的code
 # 创建下拉列表控件
 lan = ttk.Combobox(win, state='readonly')
 lan.pack()
@@ -15,7 +15,7 @@ lan.current(1)  # 默认第几项被选中
 
 def readCom():
     # 选中项目的数据,选中项目的索引
-    print(lan.get(), lan.current())
+    print(f"当前选中的是{lan.get()},其编码是{comCode[lan.current()]}")
 
 
 tk.Button(win, text='测试', command=readCom).pack()
