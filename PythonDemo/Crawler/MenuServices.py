93af7e32e9925d3c20642ee5fe8b77eebbb12e7f
Icenodb <Icenodb@gmail.com>
2021-08-23 15:44:40 +0800

    创建菜单的服务

---
+++ PythonDemo/Crawler/MenuServices.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+import FilmWindow as fw
+import baseContr as bc
+
+
+def initMenu(mainWin: tk.Tk):
+    # 创建主菜单
+    mainMenu = tk.Menu()
+    # 数据维护
+    dataMenu = tk.Menu(tearoff=0)
+    # 读取ScriptList的内容,创建数据库中表---判断建表语句文件是否存在,存在则创建,不存在则忽略
+    """
+    TODO
+      应用python 的os模块,可以解决这个问题
+      ScriptList.txt----{
+              syscode
+              emp
+              dept
+              kpi
+          }
+      syscode.sql
+      emp.sql
+      dept.sql
+    """
+
+    def createTable():
+        print()
+
+    dataMenu.add_command(label="数据库初始化", command=createTable)
+
+    def filmsManager():
+        mainWin.destroy()
+        fw.openWindow()
+
+    dataMenu.add_command(label='影片管理', command=filmsManager)
+    mainMenu.add_cascade(label='数据维护', menu=dataMenu)
+    # 系统维护
+    sysMenu = tk.Menu(tearoff=0)
+    sysMenu.add_command(label='口令维护')
+    sysMenu.add_separator()  # 一条分割线
+
+    def exitMainWindow():
+        # 销毁主窗口
+        mainWin.destroy()
+
+    sysMenu.add_command(label='退出', command=exitMainWindow)
+    mainMenu.add_cascade(label='系统维护', menu=sysMenu)
+    mainWin.config(menu=mainMenu)
+
+
+if __name__ == "__main__":
+    win = bc.newWindow("豆瓣影评分析", 7)
+    initMenu(win)
+    win.mainloop()
